e72c944c0c70adebb4759fa970501a5057695ac8
Shaobo <polarishehn@gmail.com>
2018-09-19 18:57:03 -0600

    Tried five queens. It doesn't work 😭

--- lib/sampling.rkt
+++ lib/sampling.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require "dpll.rkt")
+(require "parser.rkt")
 
 (provide (all-defined-out))
 
@@ -24,6 +25,13 @@
       [(null? clause) #f]
       [else (or (satisfying-literal? assignment (car clause))
                 (satisfying-clause? assignment (cdr clause)))])))
+
+(define satisfying-clauses?
+  (λ (assignment clauses)
+    (cond
+      [(null? clauses) #t]
+      [else (and (satisfying-clause? assignment (car clauses))
+                (satisfying-clauses? assignment (cdr clauses)))])))
 
 (define get-energy
   (λ (clauses)
@@ -58,7 +66,42 @@
     (let ([flip-a-coin (λ () (if (= (random 2) 0) #f #t))])
       (foldl (λ (var-id assignment) (hash-set assignment (+ var-id 1) (flip-a-coin))) (hash) (range var-num)))))
 
-(define init-assignment (build-init-assignment 5))
-(define clauses '((1) (2) (3) (4) (5)))
+(define init-assignment (build-init-assignment 25))
+(define clauses (parse-dimacs-file "../tests/5queens.cnf"))
+;(define clauses '((1 2 3 4 5)))
 
-(Metropolis init-assignment (λ (p) (Boltzmann-distribution ((get-energy clauses) p) 0.1)) propose 30)
+(define result (filter (λ (a) (satisfying-clauses? a clauses))
+        (map (λ (x)
+               (Metropolis init-assignment (λ (p) (Boltzmann-distribution
+                                                   ((get-energy clauses) p) 0.1)) propose 500))
+             (range 100))))
+
+(define solution->string
+  (λ (assignment size)
+    (letrec ([solution->string^ (λ (idxs)
+                                  (cond
+                                    [(null? idxs) ""]
+                                    [else
+                                     (let ([idx (+ 1 (car idxs))])
+                                       (string-append (if (hash-ref assignment idx) "X" "O")
+                                                      (string-append (if (= (modulo idx size) 0) "\n" "")
+                                                                     (solution->string^ (cdr idxs)))))]))])
+      (solution->string^ (range (hash-count assignment))))))
+
+;(foldl (λ (a r) (if (hash-ref a 2) (+ r 1) r)) 0 result)
+
+(define str-sols (map (λ (a) (solution->string a 5)) result))
+
+(define count-solutions
+  (λ (str-sols ht)
+    (cond
+      [(null? str-sols) ht]
+      [else (let ([str-sol (car str-sols)])
+              (if (hash-has-key? ht str-sol)
+                  (count-solutions
+                   (cdr str-sols)
+                   (hash-set ht str-sol
+                             (+ 1 (hash-ref ht str-sol))))
+                  (count-solutions
+                   (cdr str-sols)
+                   (hash-set ht str-sol 1))))])))
